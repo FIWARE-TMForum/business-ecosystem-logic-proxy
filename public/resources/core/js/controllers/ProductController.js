3e3ffe82d7da22ea63bdb8ee3de8d72ba6adc7c2
Jaime Pajuelo <jpajuelo@conwet.com>
2015-11-12 17:52:19 +0100

    Improve product view with search filters

--- public/resources/core/js/controllers/ProductController.js
+++ public/resources/core/js/controllers/ProductController.js
@@ -4,7 +4,59 @@
 
 angular.module('app.controllers')
     .controller('ProductListCtrl', ['$scope', '$rootScope', 'EVENTS', 'Product', function ($scope, $rootScope, EVENTS, Product) {
+
         $scope.$productList = Product.$collection;
+        $scope.$productBundleList = [];
+
+        $scope.$productTypeList = Product.TYPES;
+        $scope.$productStatusList = Product.STATUS;
+        $scope.$productBrandList = [];
+
+        $scope.filters = {
+            type: "",
+            status: "",
+            brand: ""
+        };
+
+        $scope.noResults = false;
+
+        $scope.canCreateBundle = function canCreateBundle() {
+            return  $scope.$productBundleList.length > 1;
+        };
+
+        $scope.getPicture = function getPicture($product) {
+            var i, src = "";
+
+            for (i = 0; i < $product.attachment.length; i++) {
+                if ($product.attachment[i].type == 'Picture') {
+                    return $product.attachment[i].url;
+                }
+            }
+
+            return src;
+        };
+
+        $scope.filterList = function filterList() {
+            Product.filter($scope.filters, function ($filteredList) {
+                $scope.noResults = !$filteredList.length;
+            });
+        };
+
+        $scope.selectProduct = function selectProduct($product) {
+            if ($scope.isSelected($product)) {
+                $scope.$productBundleList.splice($scope.$productBundleList.indexOf($product), 1);
+            } else {
+                $scope.$productBundleList.push($product);
+            }
+        };
+
+        $scope.isSelected = function isSelected($product) {
+            return $scope.$productBundleList.indexOf($product) != -1;
+        };
+
+        Product.getBrands(function ($brands) {
+            angular.copy($brands, $scope.$productBrandList);
+        });
     }])
     .controller('ProductView', ['$scope', '$rootScope', 'EVENTS', 'Product', function ($scope, $rootScope, EVENTS, Product) {
         Product.list(function () {
--- public/resources/core/js/services/ProductService.js
+++ public/resources/core/js/services/ProductService.js
@@ -8,10 +8,22 @@ angular.module('app.services')
 
         service = {
 
+            TYPES: {
+                PRODUCT: 'Product',
+                PRODUCT_BUNDLE: 'Product Bundle'
+            },
+
+            STATUS: {
+                ACTIVE: 'Active',
+                LAUNCHED: 'Launched',
+                RETIRED: 'Retired',
+                OBSOLETE: 'Obsolete'
+            },
+
             $collection: [],
 
             list: function list(next) {
-                var params = {};
+                var params = {'relatedParty.id': LOGGED_USER.ID};
 
                 return Product.query(params, function ($collection) {
 
@@ -19,6 +31,51 @@ angular.module('app.services')
 
                     if (next != null) {
                         next(service.$collection);
+                    }
+                }, function (response) {
+                    // TODO: onfailure.
+                });
+            },
+
+            filter: function filter(userQuery, next) {
+                var params = {'relatedParty.id': LOGGED_USER.ID};
+
+                if (userQuery.type in service.TYPES) {
+                    params.isBundle = (service.TYPES[userQuery.type] !== service.TYPES.PRODUCT);
+                }
+
+                if (userQuery.status in service.STATUS) {
+                    params.lifecycleStatus = service.STATUS[userQuery.status];
+                }
+
+                if (userQuery.brand) {
+                    params.brand = userQuery.brand;
+                }
+
+                return Product.query(params, function ($collection) {
+
+                    angular.copy($collection, service.$collection);
+
+                    if (next != null) {
+                        next(service.$collection);
+                    }
+                }, function (response) {
+                    // TODO: onfailure.
+                });
+            },
+
+            getBrands: function getBrands(next) {
+                var params = {'relatedParty.id': LOGGED_USER.ID, 'fields': 'brand'};
+
+                return Product.query(params, function ($collection) {
+                    var brandSet = {};
+
+                    $collection.forEach(function ($product) {
+                        brandSet[$product.brand] = 0;
+                    });
+
+                    if (next != null) {
+                        next(Object.keys(brandSet));
                     }
                 }, function (response) {
                     // TODO: onfailure.
