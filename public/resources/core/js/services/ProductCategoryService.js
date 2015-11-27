71c7cb8186a49327de45d1ed4be5c9b0ee8581db
Jaime Pajuelo <jpajuelo@conwet.com>
2015-11-27 12:24:34 +0100

    Update category service to get subcategories

--- public/resources/core/js/services/ProductCategoryService.js
+++ public/resources/core/js/services/ProductCategoryService.js
@@ -10,15 +10,44 @@ angular.module('app.services')
 
             $collection: [],
 
+            $collectionById: {},
+
             list: function list(next) {
                 var params = {'isRoot': true, 'fields': 'name'};
 
-                return Category.query(params, function ($collection) {
+                Category.query(params, function ($collection) {
+
+                    $collection.forEach(function ($entry) {
+                        if (!Array.isArray(service.$collectionById[$entry.id])) {
+                            service.$collectionById[$entry.id] = [];
+                        }
+                    });
 
                     angular.copy($collection, service.$collection);
 
                     if (next != null) {
                         next(service.$collection);
+                    }
+                }, function (response) {
+                    // TODO: onfailure.
+                });
+            },
+
+            get: function get($category, next) {
+                var params = {'parentId': $category.id, 'fields': 'name'};
+
+                Category.query(params, function ($collection) {
+
+                    $collection.forEach(function ($entry) {
+                        if (!Array.isArray(service.$collectionById[$entry.id])) {
+                            service.$collectionById[$entry.id] = [];
+                        }
+                    });
+
+                    angular.copy($collection, service.$collectionById[$category.id]);
+
+                    if (next != null) {
+                        next(service.$collectionById[$category.id]);
                     }
                 }, function (response) {
                     // TODO: onfailure.
