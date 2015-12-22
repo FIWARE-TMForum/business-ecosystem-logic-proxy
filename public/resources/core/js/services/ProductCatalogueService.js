4be81155d4e8ce164cfc902bc97291e56d2c2951
Jaime Pajuelo <jaimepk27@gmail.com>
2015-12-22 18:31:41 +0100

    Refactor catalogue api

--- public/resources/core/js/services/ProductCatalogueService.js
+++ public/resources/core/js/services/ProductCatalogueService.js
@@ -2,196 +2,101 @@
  *
  */
 
-angular.module('app.services')
-    .factory('Catalogue', ['$rootScope', '$resource', 'URLS', 'EVENTS', 'LIFECYCLE_STATUS', 'PARTY_ROLES', 'User', function ($rootScope, $resource, URLS, EVENTS, LIFECYCLE_STATUS, PARTY_ROLES, User) {
+angular.module('app')
+    .factory('Catalogue', function ($rootScope, $resource, $q, URLS, LIFECYCLE_STATUS, PARTY_ROLES, User) {
 
-        var Catalogue, service = {
+        var Resource, service = {
 
-            MESSAGES: {
-                CREATED: 'The catalogue <strong>{{ name }}</strong> was created successfully.',
-                UPDATED: 'The catalogue <strong>{{ name }}</strong> was updated successfully.'
-            },
+            list: function list(role, filters) {
+                var deferred = $q.defer(), params = {};
 
-            $collection: [],
+                if (angular.isObject(filters)) {
 
-            $collectionById: {},
+                    if (filters.status) {
+                        params['lifecycleStatus'] = filters.status;
+                    }
 
-            list: function list(next, cached) {
-                var params = {};
-
-                if (typeof cached !== 'boolean') {
-                    cached = true;
+                    if (filters.role) {
+                        params['relatedParty.role'] = filters.role;
+                    }
                 }
 
-                switch (User.getRole()) {
+                switch (role) {
                 case User.ROLES.CUSTOMER:
-                    params = {'lifecycleStatus': LIFECYCLE_STATUS.LAUNCHED};
+                    params['lifecycleStatus'] = LIFECYCLE_STATUS.LAUNCHED;
                     break;
                 case User.ROLES.SELLER:
-                    params = {'relatedParty.id': User.getID()};
+                    params['relatedParty.id'] = User.current.id;
                     break;
-                default:
-                    // TODO: do nothing.
                 }
 
-                Catalogue.query(params, function ($collection) {
-
-                    if (cached) {
-                        angular.copy($collection, service.$collection);
-
-                        service.$collection.forEach(function ($catalogue) {
-                            service.$collectionById[$catalogue.id] = $catalogue;
-                        });
-                    }
-
-                    if (next != null) {
-                        next(cached ? service.$collection : $collection);
-                    }
-                }, function (response) {
-                    // TODO: onfailure.
+                Resource.query(params, function (catalogueList) {
+                    deferred.resolve(catalogueList);
                 });
+
+                return deferred.promise;
             },
 
-            filter: function filter(userQuery, next, cached) {
-                var params = {'relatedParty.id': User.getID()};
+            exists: function exists(params) {
+                var deferred = $q.defer();
 
-                if (typeof cached !== 'boolean') {
-                    cached = true;
-                }
-
-                if (userQuery.status in LIFECYCLE_STATUS) {
-                    params['lifecycleStatus'] = LIFECYCLE_STATUS[userQuery.status];
-                }
-
-                if (userQuery.role in PARTY_ROLES) {
-                    params['relatedParty.role'] = PARTY_ROLES[userQuery.role];
-                }
-
-                Catalogue.query(params, function ($collection) {
-
-                    if (cached) {
-                        angular.copy($collection, service.$collection);
-
-                        service.$collection.forEach(function ($catalogue) {
-                            service.$collectionById[$catalogue.id] = $catalogue;
-                        });
-                    }
-
-                    if (next != null) {
-                        next(cached ? service.$collection : $collection);
-                    }
-                }, function (response) {
-                    // TODO: onfailure.
+                Resource.query(params, function (catalogueList) {
+                    deferred.resolve(!!catalogueList.length);
                 });
+
+                return deferred.promise;
             },
 
-            find: function find(params, next, cached) {
-
-                if (typeof cached !== 'boolean') {
-                    cached = true;
-                }
-
-                Catalogue.query(params, function ($collection) {
-
-                    if (cached) {
-                        angular.copy($collection, service.$collection);
-                    }
-
-                    if (next != null) {
-                        next(cached ? service.$collection : $collection);
-                    }
-                }, function (response) {
-                    // TODO: onfailure.
-                });
-            },
-
-            hasRoleAs: function hasRoleAs($catalogue, partyRole) {
-                return $catalogue.relatedParty.some(function (party) {
-                    return party.id == User.getID() && party.role == partyRole;
-                });
-            },
-
-            create: function create(data, next, cached) {
-
-                if (typeof cached !== 'boolean') {
-                    cached = true;
-                }
+            create: function create(data) {
+                var deferred = $q.defer();
 
                 angular.extend(data, {
                     lifecycleStatus: LIFECYCLE_STATUS.ACTIVE,
                     relatedParty: [User.serialize()]
                 });
 
-                Catalogue.save(data, function ($catalogueCreated) {
-
-                    if (cached) {
-                        service.$collection.unshift($catalogueCreated);
-                        $rootScope.$broadcast(EVENTS.MESSAGE_SHOW, 'success', service.MESSAGES.CREATED, $catalogueCreated);
-                    }
-
-                    if (next != null) {
-                        next($catalogueCreated);
-                    }
-                }, function (response) {
-                    // TODO: onfailure.
+                Resource.save(data, function (catalogueCreated) {
+                    deferred.resolve(catalogueCreated);
                 });
+
+                return deferred.promise;
             },
 
-            update: function update($catalogue, next, cached) {
-                var index = service.$collection.indexOf(service.$collectionById[$catalogue.id]);
+            get: function get(catalogueId) {
+                var deferred = $q.defer(), params = {
+                    catalogueId: catalogueId
+                };
 
-                if (typeof cached !== 'boolean') {
-                    cached = true;
-                }
-
-                Catalogue.update({id: $catalogue.id}, $catalogue, function ($catalogueUpdated) {
-
-                    if (cached) {
-                        angular.copy($catalogueUpdated, service.$collection[index]);
-                        service.$collectionById[$catalogueUpdated.id] = service.$collection[index];
-
-                        $rootScope.$broadcast(EVENTS.MESSAGE_SHOW, 'success', service.MESSAGES.UPDATED, $catalogueUpdated);
-                    }
-
-                    if (next != null) {
-                        next(cached ? service.$collection[index] : $catalogueUpdated);
-                    }
-                }, function (response) {
-                    // TODO: onfailure.
+                Resource.get(params, function (catalogueRetrieved) {
+                    deferred.resolve(catalogueRetrieved);
                 });
+
+                return deferred.promise;
             },
 
-            updateStatus: function updateStatus($catalogue, lifecycleStatus, next, cached) {
-                var index = service.$collection.indexOf(service.$collectionById[$catalogue.id]);
+            update: function update(catalogue) {
+                var deferred = $q.defer(), params = {
+                    catalogueId: catalogue.id
+                };
 
-                if (typeof cached !== 'boolean') {
-                    cached = true;
-                }
+                Resource.update(params, catalogue, function (catalogueUpdated) {
+                    deferred.resolve(catalogueUpdated);
+                });
 
-                $catalogue.lifecycleStatus = lifecycleStatus;
+                return deferred.promise;
+            },
 
-                Catalogue.update({id: $catalogue.id}, $catalogue, function ($catalogueUpdated) {
-
-                    if (cached) {
-                        angular.copy($catalogueUpdated, service.$collection[index]);
-                        service.$collectionById[$catalogueUpdated.id] = service.$collection[index];
-
-                        $rootScope.$broadcast(EVENTS.MESSAGE_SHOW, 'success', service.MESSAGES.UPDATED, $catalogueUpdated);
-                    }
-
-                    if (next != null) {
-                        next(cached ? service.$collection[index] : $catalogueUpdated);
-                    }
-                }, function (response) {
-                    // TODO: onfailure.
+            hasRoleAs: function hasRoleAs(catalogue, role) {
+                return catalogue.relatedParty.some(function (party) {
+                    return party.id == User.current.id && party.role == role;
                 });
             }
 
         };
 
-        Catalogue = $resource(URLS.CATALOGUE_MANAGEMENT + '/catalog/:catalogueId', {catalogueId: '@id'}, {
-            update: {method:'PUT'}
+        Resource = $resource(URLS.CATALOGUE_MANAGEMENT + '/catalog/:catalogueId', {catalogueId: '@id'}, {
+            update: {method: 'PUT'}
         });
 
         return service;
-    }]);
+    });
