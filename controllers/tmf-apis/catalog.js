2d801d5ce244bb4af57d67628ff348d0ac6415a3
Aitor Magán <amagan@conwet.com>
2016-01-12 17:40:16 +0100

    (Proxy) Offerings can only be launched when the attached product and catalog are launched

--- controllers/tmf-apis/catalog.js
+++ controllers/tmf-apis/catalog.js
@@ -7,18 +7,18 @@ var async = require('async'),
     tmfUtils = require('./../../lib/tmfUtils'),
     log = require('./../../lib/logger').logger.getLogger("Root");
 
+var LIFE_CYCLE = 'lifecycleStatus';
+var LAUNCHED = 'launched';
+
 // Validator to check user permissions for accessing TMForum resources
 var catalog = (function() {
 
-    // Retrieves the product belonging to a given offering
-    var retrieveProduct = function(userInfo, offeringInfo, callback) {
-        var productUrl = offeringInfo.productSpecification.href;
-        var productPath = url.parse(productUrl).pathname;
+    var retrieveAsset = function(assetPath, errMsg, callback) {
 
         var options = {
             host: config.appHost,
             port: config.endpoints.catalog.port,
-            path: productPath,
+            path: assetPath,
             method: 'GET',
             headers: {'accept': 'application/json'}
         };
@@ -27,14 +27,26 @@ var catalog = (function() {
 
         http.request(protocol, options, null, function(err, result) {
             if (err) {
+
                 callback({
                     status: 400,
-                    message: 'The product specification of the given product offering is not valid'
+                    message: errMsg
                 })
             } else {
                 callback(null, result);
             }
         });
+
+    };
+
+    // Retrieves the product belonging to a given offering
+    var retrieveProduct = function(offeringInfo, callback) {
+
+        var productUrl = offeringInfo.productSpecification.href;
+        var productPath = url.parse(productUrl).pathname;
+        var errMsg = 'The product specification of the given product offering is not valid';
+
+        retrieveAsset(productPath, errMsg, callback);
     };
 
     // The request is directly allowed without extra validation required
@@ -84,7 +96,7 @@ var catalog = (function() {
         if (req.url.indexOf('productOffering') > -1) {
             // Check that the product attached to the offering is owned by
             // the same user
-            retrieveProduct(req.user, body, function(err, result) {
+            retrieveProduct(body, function(err, result) {
                if (err) {
                    callback(err);
                } else {
@@ -133,16 +145,19 @@ var catalog = (function() {
         // Retrieve the resource to be updated or removed
         http.request(protocol, options, '', function (err, result) {
             if (err) {
+
                 callback({
                     status: err.status,
                     message: err.body
                 });
+
             } else {
+
                 var parsedResp = JSON.parse(result.body);
 
                 // Check if the request is an offering
                 if (req.url.indexOf('productOffering') > -1) {
-                    retrieveProduct(req.user, parsedResp, function(err, result) {
+                    retrieveProduct(parsedResp, function(err, result) {
                         if (err) {
                             callback(err, result);
                         } else {
@@ -156,11 +171,119 @@ var catalog = (function() {
         });
     };
 
+    var checkAssetStatus = function(assetPath, status, callback) {
+
+        retrieveAsset(assetPath, 'The asset cannot be retrieved', function(err, result) {
+
+            if (err) {
+
+                callback(err);
+
+            } else {
+
+                var assetInfo = JSON.parse(result.body);
+
+                if (assetInfo[LIFE_CYCLE].toLowerCase() === status) {
+                    callback(null, true);
+                } else {
+                    callback(null, false);
+                }
+            }
+
+        });
+    };
+
+    var validateOfferingCycle = function(req, callback) {
+
+        var pattern = new RegExp('/catalog/\\d+/productOffering/\\d+');
+
+        if (pattern.exec(req.url)) {
+
+            // Check life cycle just for offerings
+
+            try {
+
+                var body = JSON.parse(req.body);
+
+                if (LIFE_CYCLE in body && body[LIFE_CYCLE].toLowerCase() == LAUNCHED) {
+
+                    var offeringPath = url.parse(req.url).pathname;
+
+                    retrieveAsset(offeringPath, 'The offering cannot be retrieved', function(err, result) {
+
+                        if (err) {
+
+                            callback(err, result);
+
+                        } else {
+
+                            var productPath = JSON.parse(result.body).productSpecification.href;
+
+                            // Check the catalog life cycle only when the offering life cycle is being updated
+
+                            var productOfferingPos = req.url.indexOf('/productOffering/');
+                            var catalogPath  = url.parse(req.url.substring(0, productOfferingPos)).pathname;
+
+                            var tasks = [];
+                            tasks.push(checkAssetStatus.bind(this, catalogPath, LAUNCHED));
+                            tasks.push(checkAssetStatus.bind(this, productPath, LAUNCHED));
+
+                            async.parallel(tasks, function(err, results) {
+
+                                if (err) {
+
+                                    callback({
+                                        status: 400,
+                                        message: 'The product and/or the catalog attached to the offering ' +
+                                                'cannot be checked'
+                                    });
+
+                                } else {
+
+                                    var valid = true;
+                                    for (var i = 0; i < results.length && valid; i++) {
+                                        valid = results[i];
+                                    }
+
+                                    if (valid) {
+
+                                        // The correct case: attached product and catalog are launched!
+                                        callback();
+                                    } else {
+
+                                        callback({
+                                            status: 400,
+                                            message: 'Offerings can only be launched when the associated catalog ' +
+                                                    'and/or product are launched'
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    });
+
+                } else {
+                    callback();
+                }
+
+            } catch (e) {
+                callback({
+                    status: 400,
+                    message: 'The resource is not a valid JSON document'
+                });
+            }
+
+        } else {
+            callback();
+        }
+
+    };
+
     var validators = {
         'GET': [ validateAllowed ],
         'POST': [ tmfUtils.validateLoggedIn, validateCreation ],
-        'PATCH': [ tmfUtils.validateLoggedIn, validateUpdate ],
-        'PUT': [ tmfUtils.validateLoggedIn, validateUpdate ],
+        'PATCH': [ tmfUtils.validateLoggedIn, validateUpdate, validateOfferingCycle ],
+        'PUT': [ tmfUtils.validateLoggedIn, validateUpdate, validateOfferingCycle ],
         'DELETE': [ tmfUtils.validateLoggedIn, validateUpdate ]
     };
 
