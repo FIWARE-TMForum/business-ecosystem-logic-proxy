766c360441e046c4bc6456991ffb9200dff10abf
Aitor Magán <amagan@conwet.com>
2016-01-21 17:54:51 +0100

    First approach with MongoDB

--- controllers/shoppingCart.js
+++ controllers/shoppingCart.js
@@ -1,35 +1,56 @@
+var CartItem = require('../db/schemas/cartItem');
+
+var connection = mongoose.connect('mongodb://localhost:27017/shoppingCartItems');
+
 var shoppingCart = (function() {
 
-    var carts = {};
+
 
     var endRequest = function(res, code, content) {
         res.statusCode = code;
-        res.write(JSON.stringify(content));
+        res.json(content);
         res.end();
     };
 
     var getCart = function(req, res) {
 
         var userName = req.user.id;
-        var cart = carts[userName] ? carts[userName] : [];
 
-        endRequest(res, 200, cart);
+        CartItem.find({ user: userName }, function(err, result) {
+
+            if (err) {
+                endRequest(res, 500, err);
+            } else {
+
+                var items = [];
+
+                result.forEach(function(cartItem) {
+                    items.push(cartItem.itemObject);
+                });
+
+                endRequest(res, 200, items);
+            }
+        });
     };
 
-    var getItem = function(req, res) {
+    var getItem = function (req, res) {
 
         var userName = req.user.id;
-        var cart = carts[userName] ? carts[userName] : [];
+        var itemId = req.params.id;
 
-        var selectedItem = cart.filter(function(item) {
-            return item.id == req.params.id;
-        })[0];
+        CartItem.findOne({ user: userName, itemId: itemId }, function(err, result) {
 
-        if (selectedItem) {
-            endRequest(res, 200, selectedItem);
-        } else {
-            endRequest(res, 404, { error: 'The given ordering item does not exist in your cart' });
-        }
+            if (err) {
+                endRequest(res, 500, err);
+            } else {
+                if (result) {
+                    endRequest(res, 200, result.itemObject);
+                } else {
+                    endRequest(res, 404, { error: 'Item not found in your cart' });
+                }
+            }
+
+        });
     };
 
     var add = function(req, res) {
@@ -37,57 +58,59 @@ var shoppingCart = (function() {
         var itemId = req.params.id;
         var userName = req.user.id;
 
-        if (!(userName in carts)) {
-            carts[userName] = [];
-        }
-
         try {
+            var item = new CartItem();
+            item.user = userName;
+            item.itemId = itemId;
+            item.itemObject = JSON.parse(req.body);
+            item.itemObject.id = itemId;
 
-            var newItem = JSON.parse(req.body);
-            newItem.id = itemId;
-
-            var itemsSameId = carts[userName].filter(function(item) {
-                return item.id == itemId;
+            item.save(function (err) {
+                if (err) {
+                    endRequest(res, 404, err);
+                } else {
+                    endRequest(res, 200, {status: 'ok'});
+                }
             });
-
-            if (itemsSameId.length > 0) {
-                endRequest(res, 409, { error: 'The shopping cart already contains this item' });
-            } else {
-                carts[userName].push(newItem);
-                endRequest(res, 200, { status: 'ok' });
-            }
-
         } catch (e) {
-            endRequest(res, 400, { error: 'The given item is not valid' });
+            endRequest(res, 400, { error: 'Invalid Cart Item' });
         }
-
     };
 
     var remove = function(req, res) {
 
-        var userName = req.user.id;
         var itemId = req.params.id;
-        var cart = carts[userName] ? carts[userName] : [];
-        var cartPreviousLength = cart.length;
+        var userName = req.user.id;
 
-        carts[userName] = cart.filter(function(item) {
-           return item.id != itemId;
+        CartItem.remove({ user: userName, itemId: itemId }, function(err, dbRes) {
+
+            if (err) {
+                endRequest(res, 500, err);
+            } else {
+
+                if (dbRes.result['n'] > 0) {
+                    endRequest(res, 200, { status: 'ok' });
+                } else {
+                    endRequest(res, 404, { error: 'The given ordering item cannot be deleted since it was not present in your cart'})
+                }
+            }
         });
-
-        if (cartPreviousLength === carts[userName].length) {
-            endRequest(res, 404, { error: 'The given ordering item cannot be deleted since it was not present in your cart'});
-        } else {
-            endRequest(res, 200, { status: 'ok'});
-        }
 
     };
 
     var empty = function(req, res) {
 
         var userName = req.user.id;
-        carts[userName] = [];
 
-        endRequest(res, 200, { status: 'ok' });
+        CartItem.remove({ user: userName }, function(err, result) {
+
+            if (err) {
+                endRequest(res, 500, err);
+            } else {
+                endRequest(res, 200, { status: 'ok' });
+            }
+
+        });
     };
 
     return {
