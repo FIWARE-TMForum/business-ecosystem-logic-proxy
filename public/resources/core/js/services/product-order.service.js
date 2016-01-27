941f525da0761c9121b46dc967b289c9df820a31
Jaime Pajuelo <jpajuelo@conwet.com>
2016-01-27 17:38:42 +0100

    Add service method to search product orders

--- public/resources/core/js/services/product-order.service.js
+++ public/resources/core/js/services/product-order.service.js
@@ -17,9 +17,75 @@
             productOrderId: '@id'
         });
 
+        resource.prototype.getCustomer = getCustomer;
+        resource.prototype.getRoleOf = getRoleOf;
+
         return {
+            search: search,
             create: create
         };
+
+        function search(filters) {
+            var deferred = $q.defer();
+            var params = {};
+
+            if (filters.owner) {
+                params['relatedParty.id'] = User.loggedUser.id;
+            }
+
+            if (filters.role) {
+                params['relatedParty.role'] = filters.role;
+            }
+
+            if (filters.status) {
+                params['state'] = filters.status;
+            }
+
+            resource.query(params, function (productOrderList) {
+                var productOfferingFilters = {};
+
+                if (productOrderList.length) {
+                    productOfferingFilters.id = getProductOfferingIds(productOrderList).join();
+
+                    Offering.search(productOfferingFilters).then(function (productOfferingList) {
+                        replaceProductOffering(productOrderList, productOfferingList);
+                        deferred.resolve(productOrderList);
+                    });
+                } else {
+                    deferred.resolve(productOrderList);
+                }
+            }, function (response) {
+                deferred.reject(response);
+            });
+
+            return deferred.promise;
+
+            function getProductOfferingIds(productOrderList) {
+                var productOfferingIds = {};
+
+                productOrderList.forEach(function (productOrder) {
+                    productOrder.orderItem.forEach(function (orderItem) {
+                        productOfferingIds[orderItem.productOffering.id] = {};
+                    });
+                });
+
+                return Object.keys(productOfferingIds);
+            }
+
+            function replaceProductOffering(productOrderList, productOfferingList) {
+                var productOfferings = {};
+
+                productOfferingList.forEach(function (productOffering) {
+                    productOfferings[productOffering.id] = productOffering;
+                });
+
+                productOrderList.forEach(function (productOrder) {
+                    productOrder.orderItem.forEach(function (orderItem) {
+                        orderItem.productOffering = productOfferings[orderItem.productOffering.id];
+                    });
+                });
+            }
+        }
 
         function create(orderInfo) {
             var deferred = $q.defer();
@@ -34,6 +100,32 @@
             });
 
             return deferred.promise;
+        }
+
+        function getCustomer() {
+            /* jshint validthis: true */
+            var i, user;
+
+            for (i = 0; i < this.relatedParty.length && !user; i++) {
+                if (this.relatedParty[i].role === 'Customer') {
+                    user = this.relatedParty[i];
+                }
+            }
+
+            return user;
+        }
+
+        function getRoleOf(userId) {
+            /* jshint validthis: true */
+            var i, role;
+
+            for (i = 0; i < this.relatedParty.length && !role; i++) {
+                if (this.relatedParty[i].id == userId) {
+                    role = this.relatedParty[i].role;
+                }
+            }
+
+            return role;
         }
     }
 
