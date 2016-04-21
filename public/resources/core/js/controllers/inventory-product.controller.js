e4eae28b7470029505e769f439e25b21c5c2d825
fdelavega <fdelavega@conwet.com>
2016-04-21 15:48:14 +0200

    Update product details view to support renewing pay-per-use and subscription pricings

--- public/resources/core/js/controllers/inventory-product.controller.js
+++ public/resources/core/js/controllers/inventory-product.controller.js
@@ -41,14 +41,19 @@
         }
     }
 
-    function ProductDetailController($scope, $state, InventoryProduct, Utils, ProductSpec) {
+    function ProductDetailController(
+        $rootScope, $scope, $state, InventoryProduct, Utils, ProductSpec, EVENTS, $interval, $window) {
         /* jshint validthis: true */
         var vm = this;
+        var load = false;
 
         vm.item = {};
         vm.$state = $state;
         vm.formatCharacteristicValue = formatCharacteristicValue;
         vm.characteristicValueSelected = characteristicValueSelected;
+        vm.isRenewable = isRenewable;
+        vm.renewProduct = renewProduct;
+        vm.loading = loading;
 
         InventoryProduct.detail($state.params.productId).then(function (productRetrieved) {
             vm.item = productRetrieved;
@@ -58,6 +63,64 @@
             vm.error = Utils.parseError(response, 'It was impossible to load product details');
             vm.item.status = ERROR;
         });
+
+        function loading() {
+            return load;
+        }
+
+        function isRenewable() {
+            return 'productPrice' in vm.item && vm.item.productPrice.length && 'priceType' in vm.item.productPrice[0] &&
+                (vm.item.productPrice[0].priceType.toLowerCase() == 'recurring'
+                || vm.item.productPrice[0].priceType.toLowerCase() == 'usage')
+        }
+
+        function renewProduct() {
+            load = true;
+            InventoryProduct.renew({
+                name: vm.item.name,
+                id: vm.item.id,
+                priceType: vm.item.productPrice[0].priceType.toLowerCase()
+            }).then(function(reviewJob) {
+                load = false;
+                if ('x-redirect-url' in reviewJob.headers) {
+                    var ppalWindow = $window.open(reviewJob.headers['x-redirect-url'], '_blank');
+                    var interval;
+
+                    // The function to be called when the payment process has ended
+                    var paymentFinished = function(closeModal) {
+
+                        if (interval) {
+                            $interval.cancel(interval);
+                        }
+
+                        if (closeModal) {
+                            $rootScope.$emit(EVENTS.MESSAGE_CLOSED);
+                        }
+
+                    };
+
+                    // Display a message and wait until the new tab has been closed to redirect the page
+                    $rootScope.$emit(EVENTS.MESSAGE_CREATED, reviewJob.headers['x-redirect-url'], paymentFinished.bind(this, false));
+
+                    if (ppalWindow) {
+                        interval = $interval(function () {
+                            if (ppalWindow.closed) {
+                                paymentFinished(true);
+                            }
+                        }, 500);
+                    }
+                }
+            }, function (response) {
+                load = false;
+                var defaultMessage = 'There was an unexpected error that prevented the ' +
+                    'system from renewing your product';
+                var error = Utils.parseError(response, defaultMessage);
+
+                $rootScope.$broadcast(EVENTS.MESSAGE_ADDED, 'error', {
+                    error: error
+                });
+            });
+        }
 
         function characteristicValueSelected(characteristic, characteristicValue) {
             var result, productCharacteristic, i;
