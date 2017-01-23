ad9b54e5a018f3d71caee70e422da59549bbd588
fdelavega <fdelavega@conwet.com>
2017-01-23 13:53:06 +0100

    Create a volume for saving indexes in docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,4 +6,5 @@ python /entrypoint.py
 
 sleep 15
 
+/business-ecosystem-logic-proxy/node-v6.9.1-linux-x64/bin/node fill_indexes.js
 /business-ecosystem-logic-proxy/node-v6.9.1-linux-x64/bin/node server.js
