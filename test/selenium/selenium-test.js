e608a5d8ec016cfa89cb8dd76367c30a0b648129
egonzalo <egonzalo@conwet.com>
2017-04-04 13:30:31 +0200

    WiP/ fixed a ton of selenium bugs and whatnot. Not a totally working version. Planning now the pre and post conditions of the tests.

--- test/selenium/selenium-test.js
+++ test/selenium/selenium-test.js
@@ -10,8 +10,24 @@ describe('Integration tests', function () {
     var driver = new webdriver.Builder()
 	.forBrowser('chrome')
 	.build();
+
+    function checkLogin(driver, done) {
+	driver.wait(until.titleIs('Biz Ecosystem'));
+	driver.wait(until.elementLocated(By.linkText('Sign in')));
+	driver.findElement(By.linkText('Sign in')).click();
+	driver.wait(until.elementLocated(By.id('id_username')));
+	driver.findElement(By.id('id_username')).sendKeys(userProvider.id);
+	driver.findElement(By.id('id_password')).sendKeys(userProvider.pass);
+	driver.findElement(By.className('btn btn-primary pull-right')).click();
+	driver.wait(until.titleIs('Biz Ecosystem'));
+	var userLogged = driver.findElement(By.className("hidden-xs ng-binding"));
+	userLogged.getText().then(function(name){
+	    expect(name).toBe('testfiware');
+	    done();
+	});
+    };
     
-    fdescribe('User logs in, updates his/her info, creates a new category, uploads a new product and creates an offering containing that product.', function () {
+    fdescribe('User.', function () {
 	
 	beforeAll(function(done) {
 	    jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
@@ -29,24 +45,23 @@ describe('Integration tests', function () {
 	userProvider = {id: '58d5266e056d1@mailbox92.biz',
 			pass: 'test'};
 	
+
+	/*
+	  As far as i know, these test must be passed in this order as they emulate user possible actions.
+	 */
+
+
+	// Use this as a placeholder for new tests. Doing all the tests is horrible enough without worrying about what
+	// html thing should be checked.
+	// // TODO: Change this expect. This checks nothing
+	// driver.getTitle().then(function (title) {
+	//     expect(title).toBe('Biz Ecosystem');
+	// });
 	
 	driver.get('http://localhost:8000/#/offering');
 	
 	it('Should be able to log in with a correct username and password', function (done) {
-	    driver.wait(until.titleIs('Biz Ecosystem'));
-	    driver.wait(until.elementLocated(By.linkText('Sign in')));
-	    driver.findElement(By.linkText('Sign in')).click();
-	    driver.wait(until.elementLocated(By.id('id_username')));
-	    // CAUTION: CHANGE patata TO potorro BEFORE ANY TEST. 
-	    driver.findElement(By.id('id_username')).sendKeys(userProvider.id);
-	    driver.findElement(By.id('id_password')).sendKeys(userProvider.pass);
-	    driver.findElement(By.className('btn btn-primary pull-right')).click();
-	    driver.wait(until.titleIs('Biz Ecosystem'));
-	    var userLogged = driver.findElement(By.className("hidden-xs ng-binding"));
-	    userLogged.getText().then(function(name){
-		expect(name).toBe('test2');
-		done();
-	    });
+	    checkLogin(driver, done)
 	});
 
 	it('Should be able to update his/her info', function(done) {
@@ -60,11 +75,12 @@ describe('Integration tests', function () {
 	    driver.findElement(By.name('lastName')).clear();
 	    driver.findElement(By.name('lastName')).sendKeys('testSurName');
 	    driver.findElement(By.className('btn btn-success')).click();
-	    // TODO: Change this expect. This checks nothing
-	    driver.getTitle().then(function (title) {
-		expect(title).toBe('Biz Ecosystem');
+	    driver.wait(until.elementLocated(By.className('h4 text-dark-secondary')))
+	    var nameInput = driver.wait(until.elementLocated(By.name('firstName')));
+	    nameInput.getAttribute("value").then(function(firstName) {
+		expect(firstName).toBe('testName');
 		done();
-	    });
+	    })
 	});
 
 	it('Should be able to create a new category hierarchy', function(done) {
@@ -85,36 +101,71 @@ describe('Integration tests', function () {
 	    	// TODO: Change this expect. This checks nothing
 	    	driver.getTitle().then(function (title) {
 	    	    expect(title).toBe('Biz Ecosystem');
+		    done();
 	    	});
+	    }else{
+		done();
 	    }
-	    done();
 	});
 
-	it('Create a new catalog', function(done) {
+	it('Will try to create a new catalog. If the catalog already exists, he should be able to update the status of that catalog', function(done) {
 	    driver.findElement(By.className('btn btn-default z-depth-1')).click();
 	    driver.wait(until.titleIs('Biz Ecosystem'));
 	    driver.findElement(By.linkText('My stock')).click();
-	    driver.wait(until.elementLocated(By.className('btn btn-success')));
-	    driver.findElement(By.className('btn btn-success')).click();
-	    driver.wait(until.elementLocated(By.name('name')));
-	    driver.findElement(By.name('name')).sendKeys('testCatalog');
-	    driver.findElement(By.name('description')).sendKeys('A testing description');
-	    driver.wait(until.elementLocated(By.className('h4 text-dark-secondary')));
-	    driver.findElement(By.className('btn btn-warning')).click();	    
-	    driver.wait(until.titleIs('Biz Ecosystem'));
-	    var catalogName = driver.findElement(By.name("name"));
-	    catalogName.getText().then(function(name){
-		expect(name).toBe('testCatalog');
-	    });
-	    driver.wait(until.elementLocated(By.className('status-item status-launched')));
-	    driver.findElement(By.className('status-item status-launched')).click();
-	    driver.findElement(By.className('btn btn-success')).click();
-	    driver.findElement(By.linkText('Home')).click();
-	    driver.wait(until.elementLocated(By.className('panel-heading')));
-	    foundCatalog = !!driver.findElements(By.linkText('testCatalog'));
-	    expect(foundCatalog).toBe(true);
-	    done();
+	    driver.wait(until.elementLocated(By.linkText('testCatalog19')));
+	    var foundCatalog = !!driver.findElements(By.linkText('testCatalog22'));
+	    if(foundCatalog){
+		driver.findElement(By.linkText('testCatalog23')).click();
+		// driver.wait(until.elementLocated(By.className('status-item status-launched')));
+		// expect(name).toBe('testCatalog20');
+		// var icon = driver.findElement(By.css("div.status-item.status-launched"));
+		// driver
+		//     .actions()
+		//     .click(icon)
+		//     .perform();
+		driver.wait(until.elementLocated(By.className('status-item status-launched active')));
+		driver.findElement(By.className('btn btn-success')).click();
+		driver.wait(until.elementLocated(By.linkText('Home')));
+		// var home = driver.findElement(By.linkText('Home'));
+		// home.click();
+		// home.then(function(webElement){
+		//     driver.actions().mouseMove(webElement).click().perform();
+		//     driver.wait(until.elementLocated(By.className('panel-heading')));
+		//     foundCatalog = !!driver.findElements(By.linkText('testCatalog23'));
+		//     expect(foundCatalog).toBe(true);
+		// });
+		
+		// Selenium has a bug where it wont load the second instruction of a goTo("URL"). I dont even know why this work around works. But it does.
+		driver.navigate().to("chrome://version/")
+		
+		driver.navigate().to("http://localhost:8000/#/offering")
+		driver.wait(until.elementLocated(By.linkText('testCatalog23')));
+		foundCatalog = driver.findElement(By.linkText('testCatalog23'));
+		foundCatalog.then(x => x.getText().then(function(text) {
+			expect(text).toBe('testCatalog23');
+			done();
+		}));
+		
+	    }else{
+		driver.findElement(By.className('btn btn-success')).click();
+		driver.wait(until.elementLocated(By.name('name')));
+		driver.findElement(By.name('name')).sendKeys('testCatalog21');
+		driver.findElement(By.name('description')).sendKeys('A testing description');
+		driver.findElement(By.linkText('Next')).click();
+		driver.wait(until.elementLocated(By.className('h4 text-dark-secondary')));
+		driver.findElement(By.className('btn btn-warning')).click();	    
+		driver.wait(until.elementLocated(By.className('h4 text-dark-secondary')));
+		var catalogName = driver.findElement(By.name("name"));
+		catalogName.getAttribute("value").then(function(name){
+		    
+		});				       
+	    }
 	});
+
+	// xit('Create a new product Specification', function(done) {
+	    
+	//     done();
+	// });
     });
 });
 
