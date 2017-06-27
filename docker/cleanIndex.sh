e60463b45a085cac7d79742de1c71a9bd279f227
egonzalo <egonzalo@conwet.com>
2017-06-27 12:24:25 +0200

    Fix path in script

--- docker/cleanIndex.sh
+++ docker/cleanIndex.sh
@@ -3,6 +3,6 @@
 echo "Cleaning indexes"
 rm -rf /business-ecosystem-logic-proxy/indexes/*
 echo $? 2>1
-node /business-ecosystem-logic-proxy/fill_indexes.js
+/business-ecosystem-logic-proxy/node-v6.9.1-linux-x64/bin/node /business-ecosystem-logic-proxy/fill_indexes.js
 echo $? 2>1
 exit 0
