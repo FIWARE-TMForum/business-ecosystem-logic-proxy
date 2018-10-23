efeb7c85831d601d8d0d1c1141cddc0cf9ffceef
Francisco de la Vega <fdelavega@conwet.com>
2018-10-23 19:02:25 +0200

    Update version

--- controllers/management.js
+++ controllers/management.js
@@ -21,7 +21,7 @@ var management = (function() {
 
     var versionInfo = {
         version: '7.4.0',
-        releaseDate: '',
+        releaseDate: '23/10/2018',
         gitHash: '',
         doc: 'https://fiware-tmforum.github.io/Business-API-Ecosystem/v7.4.0/',
         userDoc: 'http://business-api-ecosystem.readthedocs.io/en/v7.4.0/'
