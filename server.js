bd869b404e71fc4c97621e4908b58ba89e14f95d
Francisco de la Vega <fdelavega@ficodes.com>
2020-04-27 18:07:43 +0200

    Update elasticsearch indexing to analyze texts as english

--- server.js
+++ server.js
@@ -233,6 +233,7 @@ config.usageChartURL = process.env.BAE_LP_USAGE_CHART || config.usageChartURL;
 
 config.indexes.engine = process.env.BAE_LP_INDEX_ENGINE || config.indexes.engine;
 config.indexes.elasticHost = process.env.BAE_LP_INDEX_URL || config.indexes.elasticHost;
+config.indexes.apiVersion = process.env.BAE_LP_INDEX_API_VERSION || config.indexes.apiVersion;
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
