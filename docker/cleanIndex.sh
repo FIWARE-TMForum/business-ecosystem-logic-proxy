6bf40c882fc56308d74fc853bf5d03be7db4b12b
Francisco de la Vega <fdelavega@ficodes.com>
2022-06-20 18:26:34 +0200

    Update docker to use multistage build

--- docker/cleanIndex.sh
+++ docker/cleanIndex.sh
@@ -3,6 +3,6 @@
 echo "Cleaning indexes"
 rm -rf /business-ecosystem-logic-proxy/indexes/*
 echo $? 2>1
-/business-ecosystem-logic-proxy/node-v6.9.1-linux-x64/bin/node /business-ecosystem-logic-proxy/fill_indexes.js
+node /opt/business-ecosystem-logic-proxy/fill_indexes.js
 echo $? 2>1
 exit 0
