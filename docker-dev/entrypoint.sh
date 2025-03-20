c89d2a137280491a84e0da22538ba05c7ecbaa94
Francisco de la Vega <fdelavega@ficodes.com>
2025-03-20 12:10:31 +0100

    Trigger the migration scripts if requested in docker

--- docker-dev/entrypoint.sh
+++ docker-dev/entrypoint.sh
@@ -4,4 +4,13 @@ cd /business-ecosystem-logic-proxy
 npm install
 
 echo "Proxy-dev deployed"
+
+if [ "${MIGRATE_DATA}" = "True" ]; then
+    cd migration-scripts
+    node offering_owner.js
+    node default_catalog.js
+    node offering_migration.js
+    cd ..
+fi
+
 while true; do sleep 1000; done
